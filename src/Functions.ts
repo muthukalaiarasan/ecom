7b3c639eba935442529bfa2dd9cec4ebc75c244e
Raghul7S <raghulvairavaraj@gmail.com>
2024-06-20 13:26:36 +0530

    TypeScript Basic Learn

---
+++ src/Functions.ts
@@ -0,0 +1,64 @@
+ // Typescript Functions
+
+//  Function type 
+
+//  let greet = () => {
+//     return "Hello"
+//  }
+
+// Function Parameters
+
+//  let add = (a:number, b:number) => {
+//     return a+b
+//  }
+
+// console.log(add(10,20));
+
+// Function With Return
+
+// let add = (a:number, b:number):number => {
+//     return a+b
+//  }
+
+// console.log(add(10,20));
+
+// Function With Union types
+
+// let add = (a:number, b:number, c:(string | number)):number => {
+    
+//     if(typeof c==='number'){
+//     return a + b + c
+//     }
+//     return a + b
+//  } 
+
+// console.log(add(10,20,10)); //In this function we should use if because we define the type as number or string
+
+
+// Function with optional params
+
+// let add = (a:number, b:number, c?:(string | number)):number => {
+    
+//     if(typeof c==='number'){
+//     return a + b + c
+//     }
+//     console.log(c);
+//     return a + b
+//  } 
+
+// console.log(add(10,20,10)); // Using ? to make c as a optional one
+
+
+// Funtion with default values
+
+// let add = (a:number, b:number=20, c:(string | number)=10):number => {
+    
+//     if(typeof c==='number'){
+//     return a + b + c
+//     }
+//     console.log(c);
+//     return a + b
+//  } 
+
+// console.log(add(10)); // Use = for default values
+
---
+++ src/index.ts
@@ -0,0 +1,155 @@
+console.log('TuesDay');
+
+let myName = 'Raghul'
+
+let employeeName:string = "Raghul"
+
+let count:number = 7
+
+let isPermanent:boolean = true
+
+let multiply = (num:number) => {
+    return 10 * num
+}
+
+console.log(multiply(20));
+
+
+/*Implicit start*/ 
+
+// let numArray = [1,2,3]
+// numArray.push(4)
+
+// console.log(numArray); // In push we should push number only.
+
+
+// let strArray = ["r","a","g","h","u"]
+// strArray.push("l")
+
+// console.log(strArray); // In push we shoul push only string.
+
+
+ /*Explicit start*/
+ 
+//let numArray:number[] = [1,2,3]
+//numArray.push(4)
+//
+//console.log(numArray); // To define data type we have to use :number[].
+
+
+//let strArray:string[] = ["r","a","g","h","u"]
+//strArray.push('l')
+//
+//console.log(strArray); // To define data type we have to use :string[].
+ 
+
+ /*Declare and Assign later*/
+
+//let numArray:number[] // Declared
+//
+//numArray = [] // Array Initialize
+//numArray.push(7) 
+//console.log(numArray);
+//
+//
+//
+//let strArray:string[] = [];// = [] means array initialized
+//
+//strArray.push("R")
+//console.log(strArray);
+
+
+ /* Read Only */
+
+//let strArray:readonly string[] = ["h", "i"] // Using 'readonly' we cant push values.
+
+ //Object 
+
+ //Implicit Object
+
+// let employee = {
+//      empName : "Raghul",
+//      age : 21,
+//      isPermanent : true
+// }
+
+// employee.age = 20
+// console.log(employee);
+
+ //Explicit Object
+
+// type Empolyee = {
+//     empName : string,
+//     age : number,
+//     isPermanent : boolean
+// }
+
+//  let employee:Empolyee = {
+//     empName : "Raghul",
+//     age : 21,
+//     isPermanent : true
+// }
+
+ //Optional Object
+
+// type Empolyee = {
+//    empName : string,
+//    age? : number,
+//    isPermanent : boolean
+//} // Usin ? for age we can use the object without age.
+//
+// let employee:Empolyee = {
+//    empName : "Raghul",
+//    isPermanent : true
+//} // There is no age in this object but there is no error.
+
+ // Union type (Used to declare more than one type for one data. '|' is the symbol called 'or')
+
+// Implicit Union
+
+// let unionArray = [2, "two", true]
+
+// unionArray.push(3)
+// unionArray.push("three")
+// unionArray.push(false)
+
+// console.log(unionArray);
+
+// Explicit Union
+
+// let errorCode:(string|number)
+
+// errorCode = '401'
+// errorCode = 401
+
+// let unionArray:(string|number|boolean)[] = []
+
+// unionArray.push('two')
+// unionArray.push(2)
+// unionArray.push(false)
+
+// console.log(unionArray);
+
+
+ // Any type or Dynamic type
+
+//let exName:any = "Raghul"  
+//
+//console.log(exName);
+//
+//exName = 5
+//console.log(exName);
+//
+//exName = true
+//console.log(exName); // Here using :any we can use any data types in the exName value.
+
+//Any type Array
+
+// let exArray:any[] = []
+
+// exArray.push(2)
+// exArray.push("Raghul")
+// exArray.push("Raghul7s")
+// exArray.push(true)
+// console.log(exArray);
+
